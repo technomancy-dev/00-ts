96554db35be177f97f18ac2a4c8a5843fe49dd15
Levi <levi@technomancy.dev>
2025-01-09 19:04:33 +0100

    refactor(api)!: change return values to object
    Return {data, error} instead of throwing on errors.

--- src/core.ts
+++ src/core.ts
@@ -3,7 +3,14 @@ import { Contacts } from './contacts/contacts';
 import { Emails } from './emails/emails';
 import { DoubleZeroError } from './error';
 import { Lists } from './lists/lists';
+import { Subscriptions } from './subscriptions/subscriptions';
+import { Tokens } from './tokens/tokens';
 import { GetOptions, PatchOptions, PostOptions, PutOptions } from './types';
+
+type ApiResponse<T> = {
+  data: T | null;
+  error: DoubleZeroError | null;
+};
 
 const defaultUserAgent = `00-node:${version}`;
 const userAgent =
@@ -17,8 +24,21 @@ export class DoubleZero {
   readonly emails = new Emails(this);
   readonly contacts = new Contacts(this);
   readonly lists = new Lists(this);
+  readonly tokens: Tokens | null = null;
+  readonly subscriptions: Subscriptions | null = null;
 
-  constructor(readonly params: { baseUrl: string; token?: string }) {
+  constructor(
+    readonly params: { baseUrl: string; token?: string; secretKey?: string }
+  ) {
+    if (this.params.secretKey) {
+      this.tokens = new Tokens(this, this.params.secretKey);
+      this.subscriptions = new Subscriptions(
+        this,
+        this.contacts,
+        this.lists,
+        this.tokens as Tokens
+      );
+    }
     if (!params.token) {
       if (typeof process !== 'undefined' && process.env) {
         this.params.token = process.env.DOUBLEZERO_TOKEN;
@@ -26,9 +46,12 @@ export class DoubleZero {
     }
 
     if (!this.params.token) {
-      throw new Error(
-        'Missing DoubleZero API token. Pass it to the constructor `new DoubleZero({ baseUrl: "https://example.com", token: "your_api_token" })`'
-      );
+      return {
+        data: null,
+        error: new DoubleZeroError(
+          'Missing DoubleZero API token. Pass it to the constructor `new DoubleZero({ baseUrl: "https://example.com", token: "your_api_token" })`'
+        ),
+      };
     }
 
     this.headers = new Headers({
@@ -38,27 +61,55 @@ export class DoubleZero {
     });
   }
 
-  async fetchRequest<T>(path: string, options = {}): Promise<T> {
-    const response = await fetch(`${this.params.baseUrl}/api${path}`, options);
+  async fetchRequest<T>(path: string, options = {}): Promise<ApiResponse<T>> {
+    try {
+      const response = await fetch(
+        `${this.params.baseUrl}/api${path}`,
+        options
+      );
 
-    if (!response.ok) {
-      throw new DoubleZeroError(`DoubleZero API error: ${response.statusText}`);
+      if (response.status === 204) {
+        return { data: { success: 'ok' } as T, error: null };
+      }
+
+      if (!response.ok) {
+        const data = await response.json();
+        return {
+          data: null,
+          error: new DoubleZeroError(
+            `DoubleZero API error: ${response.statusText}: ${JSON.stringify(
+              data.error
+            )}`
+          ),
+        };
+      }
+
+      if (response.headers.get('content-length') === '0') {
+        return { data: {} as T, error: null };
+      }
+
+      const data = await response.json();
+
+      if (data.error) {
+        return { data: null, error: data.error };
+      }
+
+      return { data: data.data, error: null };
+    } catch (error) {
+      return {
+        data: null,
+        error: new DoubleZeroError(
+          error instanceof Error ? error.message : 'Unknown error occurred'
+        ),
+      };
     }
-
-    if (response.headers.get('content-length') === '0') {
-      return {} as T;
-    }
-
-    const data = await response.json();
-
-    if (data.error) {
-      throw new DoubleZeroError(data.error);
-    }
-
-    return data;
   }
 
-  async post<T>(path: string, payload?: unknown, options: PostOptions = {}) {
+  async post<T>(
+    path: string,
+    payload?: unknown,
+    options: PostOptions = {}
+  ): Promise<ApiResponse<T>> {
     const requestOptions = {
       method: 'POST',
       headers: this.headers,
@@ -69,7 +120,10 @@ export class DoubleZero {
     return await this.fetchRequest<T>(path, requestOptions);
   }
 
-  async get<T>(path: string, options: GetOptions = {}) {
+  async get<T>(
+    path: string,
+    options: GetOptions = {}
+  ): Promise<ApiResponse<T>> {
     const requestOptions = {
       method: 'GET',
       headers: this.headers,
@@ -79,7 +133,11 @@ export class DoubleZero {
     return await this.fetchRequest<T>(path, requestOptions);
   }
 
-  async put<T>(path: string, payload: any, options: PutOptions = {}) {
+  async put<T>(
+    path: string,
+    payload: any,
+    options: PutOptions = {}
+  ): Promise<ApiResponse<T>> {
     const requestOptions = {
       method: 'PUT',
       headers: this.headers,
@@ -90,7 +148,11 @@ export class DoubleZero {
     return await this.fetchRequest<T>(path, requestOptions);
   }
 
-  async patch<T>(path: string, payload: any, options: PatchOptions = {}) {
+  async patch<T>(
+    path: string,
+    payload: any,
+    options: PatchOptions = {}
+  ): Promise<ApiResponse<T>> {
     const requestOptions = {
       method: 'PATCH',
       headers: this.headers,
@@ -101,7 +163,7 @@ export class DoubleZero {
     return await this.fetchRequest<T>(path, requestOptions);
   }
 
-  async delete<T>(path: string, query?: unknown) {
+  async delete<T>(path: string, query?: unknown): Promise<ApiResponse<T>> {
     const requestOptions = {
       method: 'DELETE',
       headers: this.headers,
